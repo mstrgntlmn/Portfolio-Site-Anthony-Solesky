8b512ff40166b4ecc640dd79b1849d2502314078
mstrgntlmn <mstrgntlmn@gmail.com>
2022-03-08 12:34:56 -0700

    2.3 changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("Welcome to Thunderdome!  Two men enter, one man leaves.");
+
+function menuToggle(){
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle'){
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+}
+}
